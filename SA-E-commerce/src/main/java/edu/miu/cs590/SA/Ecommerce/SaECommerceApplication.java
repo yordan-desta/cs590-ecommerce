1c68f9360d23ea6ad5f2b296da8a31639c8aad71
Yordanos Desta Tegegn <yordan.desta@gmail.com>
2021-10-05 15:12:54 -0500

    initial commit

---
+++ SA-E-commerce/src/main/java/edu/miu/cs590/SA/Ecommerce/SaECommerceApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.SA.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaECommerceApplication.class, args);
+	}
+
+}
