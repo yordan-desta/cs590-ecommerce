49d97819e373772442f9b234ca411012723c44f2
Yordanos Desta Tegegn <yordan.desta@gmail.com>
2021-10-08 17:30:40 -0500

    created cc payment service

---
+++ CCPaymentService/src/test/java/edu/miu/cs590/sa/ecommerce/CCPaymentService/CcPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.sa.ecommerce.CCPaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
