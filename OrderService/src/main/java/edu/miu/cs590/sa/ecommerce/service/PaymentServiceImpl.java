2f95d679d12c98803f7570660e520ec5ffa515bb
Natnael Addis <nati1addis@gmail.com>
2021-10-11 19:02:24 -0500

    url config modified on order-service

--- OrderService/src/main/java/edu/miu/cs590/sa/ecommerce/service/PaymentServiceImpl.java
+++ OrderService/src/main/java/edu/miu/cs590/sa/ecommerce/service/PaymentServiceImpl.java
@@ -1,17 +1,18 @@
 package edu.miu.cs590.sa.ecommerce.service;
 
-import edu.miu.cs590.sa.ecommerce.domain.PaymentType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
-    @Value("${payment.url.paypal}")
-    private String paypalPaymentServiceUri;
+    private final String paypalPaymentServiceUri;
+    private final String ccPaymentServiceUri;
 
-    @Value("${payment.url.cc}")
-    private String ccPaymentServiceUri;
+    public PaymentServiceImpl(@Value("${payment.url.paypal}") String paypalPaymentServiceUri, @Value("${payment.url.cc}") String ccPaymentServiceUri) {
+        this.paypalPaymentServiceUri = paypalPaymentServiceUri;
+        this.ccPaymentServiceUri = ccPaymentServiceUri;
+    }
 
     @Override
     public String getPaypalPaymentUri() {
